1d25018976bafbecf9d6953146547f9746afd634
Callum Byrne <callumjbyrne97@gmail.com>
2021-08-24 15:53:16 +1000

    changed book constructor to class

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ let myLib = JSON.parse(localStorage.getItem("myLib")) || [];
 
 const bookContainer = document.querySelector(".bookContainer");
 
-//constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
 };
 
 
